a62f2173126993bd9682d8885472273d66786dc5
markobezic <marko.bezic@gmail.com>
2021-01-25 12:33:21 -0500

    clear button returns display to zero and pressing numbers adds them to display

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-document.getElementById("clear").addEventListener("click", () => {
-  document.getElementById("display").innerText = 0;
+let clear = document.getElementById("clear");
+let display = document.getElementById("display");
+let btn = document.getElementsByClassName("btn");
+
+clear.addEventListener("click", () => {
+  display.innerText = 0;
 });
+
+for (let i = 0; i < btn.length; i++) {
+  btn[i].addEventListener("click", () => {
+    if (btn[i].innerText >= 0 && btn[i].innerText <= 9) {
+      if (display.innerText === "0") {
+        display.innerText = "";
+      }
+      display.innerText += btn[i].innerText;
+    }
+  });
+}
+
+console.log(display.innerText);
